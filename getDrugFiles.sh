feb804e5804fee201c9106f7c42d128b9a854027
Stephen Laws <51342761+sslaws@users.noreply.github.com>
2023-06-29 12:45:16 -0700

    Create getDrugFiles.sh

---
+++ getDrugFiles.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_ap.zip
+wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles.zip
+wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_ia.zip
+wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_dr.zip
