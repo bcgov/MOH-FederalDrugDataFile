f73512dfe8bd525f22cabdb534cac1108da0dcde
Stephen Laws <stephen.laws@nomostech.com>
2023-06-29 13:10:47 -0700

    Overwrite existing files

--- getDrugFiles.sh
+++ getDrugFiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_ap.zip
-wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles.zip
-wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_ia.zip
-wget https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_dr.zip
+wget -O allfiles_ap.zip https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_ap.zip
+wget -O allfiles.zip https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles.zip
+wget -O allfiles_ia.zip https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_ia.zip
+wget -O allfiles_dr.zip https://www.canada.ca/content/dam/hc-sc/documents/services/drug-product-database/allfiles_dr.zip
